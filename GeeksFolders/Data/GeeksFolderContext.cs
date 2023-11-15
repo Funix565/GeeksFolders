d85ae4b193bcff8abb43ff3c87fd3ea0379e5964
Funix565 <denkin.dmitriy@gmail.com>
2023-11-16 01:22:37 +0200

    feat: create model with self-referencing relationship and configure dbcontext

---
+++ GeeksFolders/Data/GeeksFolderContext.cs
@@ -0,0 +1,28 @@
+﻿using GeeksFolders.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeeksFolders.Data
+{
+    public class GeeksFolderContext : DbContext
+    {
+        public GeeksFolderContext(DbContextOptions<GeeksFolderContext> options) : base(options)
+        {            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<GeekFolder>()
+                .HasOne(f => f.Parent)
+                .WithMany(f => f.Children)
+                .HasForeignKey(f => f.ParentId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // Unique constraint to ensure each child's name is unique withing its parent
+            modelBuilder.Entity<GeekFolder>()
+                .HasIndex(f => new { f.ParentId, f.Name })
+                .IsUnique();
+        }
+
+        public DbSet<GeekFolder> GeekFolders { get; set; }
+    }
+}
---
+++ GeeksFolders/Models/GeekFolder.cs
@@ -0,0 +1,14 @@
+﻿namespace GeeksFolders.Models
+{
+    public class GeekFolder
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        // Self-referencing relationship
+        public int? ParentId { get; set; }
+        public GeekFolder? Parent { get; set; }
+
+        public ICollection<GeekFolder>? Children { get; set; }
+    }
+}
